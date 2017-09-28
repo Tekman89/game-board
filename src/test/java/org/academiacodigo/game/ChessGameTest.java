24dfe72f384c9dd593323d610fbd8f8c9c55d77a
Jorge <tekman89@gmail.com>
2017-09-28 18:06:25 +0100

    changed gitignore

---
+++ src/test/java/org/academiacodigo/game/ChessGameTest.java
@@ -0,0 +1,7 @@
+package org.academiacodigo.game;
+
+/**
+ * Created by tekman on 20/04/2017.
+ */
+public class ChessGameTest {
+}
---
+++ src/test/java/org/academiacodigo/game/piece/chess/ChessPieceTest.java
@@ -0,0 +1,7 @@
+package org.academiacodigo.game.piece.chess;
+
+/**
+ * Created by tekman on 20/04/2017.
+ */
+public class ChessPieceTest {
+}
